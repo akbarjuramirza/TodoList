6ed2c00c425151a48e47aa63eb729eff34305cf0
AJ <a.juramirza@gmail.com>
2023-08-08 16:22:48 +0500

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Akbarjon Juramirzaev on 08/08/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
